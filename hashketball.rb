182eea97211559b00262c4f6dec2c682de3e876a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 22:57:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -231,7 +231,7 @@ end
 
 def long_name_steals_a_ton?()
   most_steals = 0
-  player_with_most_steals = nil
+  player_with_most_steals = ?
   
   game_hash().each do |location, team_data|
     team_data[:players].each do |player_name, player_stats|
